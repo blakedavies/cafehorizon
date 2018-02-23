07bec072718e505083580b09bd75d1feddf4e358
Blake Davies <blakedavies1992@gmail.com>
2018-02-24 00:16:51 +1000

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -1,8 +1,4 @@
-	
-		var val = $("#entry.1594840891").val();
-		if (val = 'TEST') {
-        
-        $('#gform').submit(function(e) {
+$('#gform').submit(function(e) {
 	      e.preventDefault();
 	      $.ajax({
 	        url: "https://docs.google.com/a/blakedavies.net/forms/d/e/1FAIpQLSdlElv_pBFNUwP7OZ4wI-BLIA_QLDMjxa1v3UwvucJCjuSVbg/formResponse",
@@ -17,18 +13,10 @@
 	        }
 	      });
 	    });
-
-		$('#gform').on('submit', function(e) {
+	    $('#gform').on('submit', function(e) {
 	  		$('#gform *').fadeOut(0);
 	  		$('#contact-box *').fadeOut(0);
 	  		$('#gform').prepend(
 	    		'<div class="thank-you-box"><h3 class="thank-you">Thank-you!!<h3><p class="thank-you">This is a website template.  Please contact Blake Davies directly via his website at http://blakedavies.net/ .</p></div>'
 	    	);
 	  	});
-
-    }
-    else {
-    	$('#gform').prepend(
-	    		'<div class="thank-you-box"><h3 class="thank-you">Thank-you!!<h3><p class="thank-you">This is a website template.  Please contact Blake Davies directly via his website at http://blakedavies.net/ .</p></div>'
-	    	);
-    }	
