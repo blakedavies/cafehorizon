690c3c06fa2f37037dda4d008768d359c2307b2a
Blake Davies <blakedavies1992@gmail.com>
2018-02-24 03:10:12 +1000

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -1,7 +1,6 @@
 $('form').submit(function(e) {
       if (parseInt($('input[name="entry.82829002"]').val()) == 12) {
         return true;
-	      e.preventDefault();
 	      $.ajax({
 	        url: "https://docs.google.com/a/blakedavies.net/forms/d/e/1FAIpQLSdlElv_pBFNUwP7OZ4wI-BLIA_QLDMjxa1v3UwvucJCjuSVbg/formResponse",
 	        data: $(this).serialize(),
